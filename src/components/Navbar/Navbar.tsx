718f7a0d79447c68c7f26a04ef901224bc634f45
Jose Vasconcelos <tadeo199928j@gmail.com>
2026-03-14 18:18:58 +0000

    bug fixing

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,9 +1,11 @@
 import { Link, useLocation } from "react-router-dom";
 import "./Navbar.css";
 import { AiOutlineThunderbolt } from "react-icons/ai";
+import { useEffect, useState } from "react";
 
 function Navbar() {
   const location = useLocation();
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
   const isSession = location.pathname.startsWith("/session/");
 
   const handledMenu = () => {
@@ -11,20 +13,26 @@ function Navbar() {
     menu?.classList.toggle("active");
   };
 
+  useEffect(() => {
+    setIsAuthenticated(!!localStorage.getItem("token"));
+  }, [location.pathname]);
+
   return (
     <nav className="navbar">
       <div className="Title">
         <h1>Caja de Arena</h1>
       </div>
-      {!isSession && (
+      {!isSession && isAuthenticated && (
         <div className="buttons">
+          <Link to="/dashboard">Dashboard</Link>
           <Link to="/cajadeArena">Caja de Arena</Link>
           <Link to="/seleccionDioses">Seleccion de Dioses</Link>
         </div>
       )}
       <AiOutlineThunderbolt className="icon" onClick={handledMenu} />
-      {!isSession && (
+      {!isSession && isAuthenticated && (
         <div className="mobile-menu">
+          <Link to="/dashboard">Dashboard</Link>
           <Link to="/cajadeArena">Caja de Arena</Link>
           <Link to="/seleccionDioses">Seleccion de Dioses</Link>
         </div>
