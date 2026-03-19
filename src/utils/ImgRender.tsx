9d6bee4afe1864df593f7ab68f3527779c3b4b45
Jose Vasconcelos <tadeo199928j@gmail.com>
2026-03-19 17:16:53 +0000

    hot fix

--- src/utils/ImgRender.tsx
+++ src/utils/ImgRender.tsx
@@ -1,11 +1,19 @@
+type GodImageModule = { default: string };
+
+const modules = import.meta.glob("../assets/*.jpg", {
+  eager: true,
+}) as Record<string, GodImageModule>;
+
 export interface ImgRender {
   id: number;
   img: string;
 }
 
-export const imgOfGods: ImgRender[] = [
-  ...Array.from({ length: 106 }, (_, i) => ({
-    id: i + 1,
-    img: new URL(`../assets/image${i + 1}.jpg`, import.meta.url).href,
-  })),
-];
+export const imgOfGods: ImgRender[] = Object.entries(modules)
+  .map(([path, mod]) => {
+    const match = path.match(/image(\d+)\.jpg$/);
+    const id = match ? Number(match[1]) : 0;
+    return { id, img: mod.default };
+  })
+  .filter((x) => x.id > 0)
+  .sort((a, b) => a.id - b.id);
